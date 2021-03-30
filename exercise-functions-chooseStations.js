941be1cff7b5333767646c4be00e976ff3b5ee23
Jennifer Lam <lam-jennifer@outlook.com>
2021-03-30 19:09:07 +0000

    exercise-functions-chooseStations initial commit

---
+++ exercise-functions-chooseStations.js
@@ -0,0 +1,26 @@
+/* Your function will receive an array of stations, where each station itself is an array with a name, a capacity, and a venue type.
+
+In order for a station to be deemed appropriate, it must have a capacity of at least 20, and be a school or community centre.
+
+The chooseStations function should return the array:
+
+['Bright Lights Elementary', 'Moose Mountain Community Centre'] */
+
+const stations = [
+  ['Big Bear Donair', 10, 'restaurant'],
+  ['Bright Lights Elementary', 50, 'school'],
+  ['Moose Mountain Community Centre', 45, 'community centre']
+];
+
+function chooseStations(stations) {
+  for (let station of stations) {
+    const capacity = station[1];
+    const venueType = station[2];
+
+    if (capacity >= 20 && (venueType === 'school' || venueType === 'community centre')) {
+      console.log(station[0])
+    }
+  }
+}
+
+chooseStations(stations);
